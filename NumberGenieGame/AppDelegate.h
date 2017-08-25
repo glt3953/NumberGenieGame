cc22704a0fc9318373c12c2a75683b2d72404f25
guoliting <guoliting@didichuxing.com>
2017-08-25 15:57:12 +0800

    初始化工程

---
+++ NumberGenieGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NumberGenieGame
+//
+//  Created by guoliting on 2017/8/25.
+//  Copyright © 2017年 NingXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NumberGenieGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NumberGenieGame
+//
+//  Created by guoliting on 2017/8/25.
+//  Copyright © 2017年 NingXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
